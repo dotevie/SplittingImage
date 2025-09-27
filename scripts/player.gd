129ef6f90a2e7ccb0f27b9a8717e98fc8e47f75b
Chase R <chasethesuper@gmail.com>
2025-09-27 02:09:00 -0400

    Input print tests

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,4 +7,18 @@ func _process(delta: float) -> void:
 	cam.position.y = self.position.y;
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	pass;
+	var velocity := state.get_linear_velocity()
+	var step := state.get_step()
+	
+	# player input
+	var move_left := Input.is_action_pressed("move_left")
+	var move_right := Input.is_action_pressed("move_right")
+	var jump := Input.is_action_pressed("jump")
+	
+	if move_left:
+		print("left")
+	if move_right:
+		print("right")
+	if jump:
+		print("jump")
+	
