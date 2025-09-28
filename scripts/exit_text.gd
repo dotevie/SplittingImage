f75ecef312a1f6a41c17a1418585865e3e47ee62
Chase R <chasethesuper@gmail.com>
2025-09-28 12:33:02 -0400

    esc quits (not in web though)

--- scripts/exit_text.gd
+++ scripts/exit_text.gd
@@ -4,9 +4,12 @@ extends Sprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if OS.has_feature("web"):
-		set_process(false)
+		queue_free()
+	if OS.has_feature("debug"):
+		print("debug build")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event):
+	if event.is_action_pressed("exit"):
+		get_tree().quit()
